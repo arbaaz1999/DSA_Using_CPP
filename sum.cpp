8e2403ecb8af64b012c395dda01d0341fbf09320
Arbaaz Mansuri <arbaazmansuri09@gmail.com>
2023-08-01 18:05:46 +0530

    dsa using cpp

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x, y, z;
+    cout<<"Enter two numbers";
+    cin>>x>>y;
+    z = x + y;
+    cout<<"Sum of above numbers is "<< z <<endl;
+    return 0;
+}
