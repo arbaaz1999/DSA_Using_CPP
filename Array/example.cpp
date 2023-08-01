0c511e7367d2db2b219ed334e565ec47586ae588
Arbaaz Mansuri <arbaazmansuri09@gmail.com>
2023-08-01 18:34:41 +0530

    prettier

--- Array/example.cpp
+++ Array/example.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main () {
+int main()
+{
     int arr[5] = {2, 4, 6, 8, 10};
     int i;
-    for (i = 0; i<5; i++) {
-        cout<<arr[i]<<endl;
-    } 
+    for (i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
     return 0;
-
 }
