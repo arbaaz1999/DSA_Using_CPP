964021dd113ddc65024bf32ccc56a0c6c3b6b2a3
Arbaaz Mansuri <arbaazmansuri09@gmail.com>
2023-08-01 18:45:01 +0530

    basic cpp formatted

--- Basics/sum.cpp
+++ Basics/sum.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main()
+{
     int x, y, z;
-    cout<<"Enter two numbers";
-    cin>>x>>y;
+    cout << "Enter two numbers";
+    cin >> x >> y;
     z = x + y;
-    cout<<"Sum of above numbers is "<< z <<endl;
+    cout << "Sum of above numbers is " << z << endl;
     return 0;
 }
