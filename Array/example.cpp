2457b04a5a677bcc09a22ef449052346b01479bd
Arbaaz Mansuri <arbaazmansuri09@gmail.com>
2023-08-01 18:25:40 +0530

    first array program written

---
+++ Array/example.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int arr[5] = {2, 4, 6, 8, 10};
+    int i;
+    for (i = 0; i<5; i++) {
+        cout<<arr[i]<<endl;
+    } 
+    return 0;
+
+}
