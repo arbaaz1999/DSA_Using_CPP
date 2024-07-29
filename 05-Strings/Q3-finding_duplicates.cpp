5c8518924cfbdc3bfd09569ab908ace9642d9859
arbaaz1999 <arbaazmansuri09@gmail.com>
2024-07-29 11:08:55 +0530

    [Problem-03]: Find Duplicates in a String

---
+++ 05-Strings/Q3-finding_duplicates.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Problem: To find the duplicates in a string using bitwise operator;
+*/
+
+int main()
+{
+    char s[] = "finding";
+    int n = sizeof(s) / sizeof(s[0]);
+    long int h = 0, a = 0;
+    for (int i = 0; i < n; i++)
+    {
+        a = 1;
+        a = a << (s[i] - 97);
+        if ((a & h) > 0)
+            cout << s[i] << " ";
+        else
+            h = h | a;
+    }
+
+    return 0;
+}
